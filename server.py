6721eab03c94c8b60b9654d65a027e55e9a4733c
Kirsten Kwong <kskwong@ualberta.ca>
2019-01-16 14:43:10 -0700

    Added a class for responses, tried sending main index page

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
@@ -26,13 +26,62 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+# defining constants
+ENCODING = "utf-8"  # encoding
+VERSION = "HTTP/1.1"  # HTTP version
+
+
+class Status:
+    METHOD_NOT_ALLOWED = "405 Method Not Allowed"
+    NOT_FOUND = "404 Not Found"
+    OK = "200 OK"
+
+
+class Response:
+    # content-type
+    # Date RFC 1123
+    # Expect
+
+    status_line = None
+    content = None
+    content_type = "text/plain"
+
+    def __init__(self, **kwargs):
+        self.status = kwargs.get("status", Status.OK)
+        self.content = kwargs.get("content", None)
+        self.content_type = kwargs.get("content_type", "text/plain")
+        self.headers = (kwargs.get("headers"), None)
+
+    def getHeader(self):
+        return bytearray("%s %s \r\n\r\n" % (VERSION, self.status), ENCODING)
+    
+    def getContent(self):
+        "Content-Type: %s \n " % (self.content_type)
+        return bytearray(self.content, ENCODING)
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        
+        # parse the request
+        request_info = self.data.decode('utf-8').split()[:2]
+        print("%s %s" % (request_info[0], request_info[1]))
+
+        if request_info[0] != "GET":
+            response = Response(
+                status=Status.METHOD_NOT_ALLOWED, 
+                content=Status.METHOD_NOT_ALLOWED
+                )
+        
+        else:
+            f = open("www/index.html")
+            response = Response(content=f.read(), content_type="text/html")
+        
+        # send the response.
+        self.request.sendall(response.getHeader())
+        self.request.sendall(response.getContent())
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
